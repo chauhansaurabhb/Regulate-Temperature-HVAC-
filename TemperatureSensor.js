2d3f99ad4e7ec71fe64be680cee0ac2e432f68d6
Saurabh Chauhan <chauhan.saurabh.b@gmail.com>
2016-06-29 11:55:46 +0530

    Updated Code for TemperatureSensor.js

--- TemperatureSensor.js
+++ TemperatureSensor.js
@@ -1,11 +1,22 @@
-var mqtt=require('mqtt');
-var client=mqtt.connect('mqtt://test.mosquitto.org:1883');
-function randomInt (low, high) {
-    return Math.floor(Math.random() * (high - low) + low);
+var sensorLib = require('node-dht-sensor'); 
+var mqtt=require('mqtt'); 
+var client=mqtt.connect('mqtt://test.mosquitto.org:1883');  
+var sensor = {
+    initialize: function () {
+        return sensorLib.initialize(22, 4); // here GPIO4 means pin7 
+    },
+    read: function () {
+        var readout = sensorLib.read();
+       var value={"tempValue":readout.temperature.toFixed(2),"unitOfMeasurement":"C"}; 
+       	client.publish('tempMeasurement',JSON.stringify(value));  
+	console.log('Current temperature is: ' + readout.temperature.toFixed(2) + 'C'); 
+        setTimeout(function () {
+            sensor.read();
+        }, 5000);
+    }
+}; 
+if (sensor.initialize()) {
+    sensor.read();
+} else {
+    console.warn('Failed to initialize sensor');
 }
-
-setInterval(function(){
-	var value={"tempValue":randomInt(20,40),"unitofMeasurement":"C"};
-	client.publish('tempMeasurement',JSON.stringify(value));	
-	console.log("Publishing Data "+value.tempValue +" "+ value.unitofMeasurement);
-},5000);
